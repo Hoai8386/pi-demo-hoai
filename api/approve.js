9c2f9fa4bfdaa8fa2eaed549d4fd9052f15d147d
Hoai8386 <hoaiphamhktn1988@gmail.com>
2026-02-26 20:31:41 +0700

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
-    
     const { paymentId } = req.body;
-    // THAY THẾ dòng dưới bằng API Key lấy từ develop.pi (App Wallet)
-    const PI_API_KEY = "MÃ_API_KEY_CỦA_BẠN": 57p6zyluq6yk5spkjzodusftz11yvxiisng960mq4khtzcawwzz6nuwo7at9bexb
+    // Dán API KEY thật lấy từ develop.pi vào đây
+    const PI_API_KEY = "57p6zyluq6yk5spkjzodusftz11yvxiisng960mq4khtzcawwzz6nuwo7at9bexb; 
 
     try {
         const response = await fetch(`https://api.minepi.com{paymentId}/approve`, {
